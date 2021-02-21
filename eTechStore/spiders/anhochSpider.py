9099a6aec2c43f12d1829ff464b32e0dc65c0309
mihail-sofeski <mihail@loka.com>
2021-02-21 14:16:32 +0100

    clear unused code

--- eTechStore/spiders/anhochSpider.py
+++ eTechStore/spiders/anhochSpider.py
@@ -1,8 +1,5 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
-from eTechStore.items import ETechStoreItem, UrlItem
-from eTechStore import general
-import pprint
+from eTechStore.items import UrlItem
 
 class AnhochSpider(scrapy.Spider):
 
@@ -38,22 +35,3 @@ class AnhochSpider(scrapy.Spider):
         next_url = response.xpath("//div[contains(@class, 'pagination')]/ul/li//i[@class='icon-angle-right']/../@href").get()
         if next_url:
             yield  scrapy.Request(next_url, callback=self.parse_category_products)
-
-# if __name__ == "__main__":
-#     dict_settings = {
-#         # 'USER_AGENT': 'eShop (+http://www.mydomain.com)',
-#         # # 'LOG_LEVEL': 'INFO',
-#         # 'ROBOTSTXT_OBEY': True,
-#         # 'COMPRESSION_ENABLED': False,
-#         # 'CONCURRENT_REQUESTS': 1,
-#         # 'AUTOTHROTTLE_ENABLED': True
-#     }
-#
-#     from scrapy.utils.project import get_project_settings
-#
-#     project_settings = get_project_settings()
-#     # project_settings.update(dict_settings)
-#
-#     process =  CrawlerProcess(project_settings)
-#     process.crawl(AnhochSpider)
-#     process.start()
